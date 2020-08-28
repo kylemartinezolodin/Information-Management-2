a847fb7d7a55a5fb4401124f84391583dc58f40e
Nathan Owen Awit <awitnathan@gmail.com>
2020-08-28 09:27:29 +0800

    Broken layout, needs fixes, and add a stepper

--- registration.php
+++ registration.php
@@ -1,11 +1,127 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap 4 Example</title>
+  <title>Register</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style>
+    body {
+  margin: 0;
+  padding: 0;
+  background-color: #17a2b8;
+  height: 100vh;
+}
+#login .container #login-row #login-column #login-box {
+  margin-top: 120px;
+  max-width: 600px;
+  height: 320px;
+  border: 1px solid #9C9C9C;
+  background-color: #EAEAEA;
+}
+#login .container #login-row #login-column #login-box #login-form {
+  padding: 20px;
+}
+#login .container #login-row #login-column #login-box #login-form #register-link {
+  margin-top: -85px;
+}
+</style>
 </head>
+<body> 
+<div id="login">
+  <div class="container">
+    <div class="row justify-content-center align-items-center" >
+      <div class="col-8 mx-auto">
+           
+            <h2 class="text-center">Registration</h2>
+        <div class="d-flex justify-content-left py-2" id="name"><h5>Name:</h5></div>
+          <div class="d-flex justify-content-center input-group">
+            <div class="row align-items-center h-100">
+              <div class="col">
+            <input type="text" class="form-control" id="firstname" placeholder="First Name"/>
+              </div>
+              <div class="col">
+            <input type="text" class="form-control" placeholder="Middle Name">
+              </div>
+              <div class="col">
+            <input type="text" class="form-control" placeholder="Last Name">
+            </div>
+          </div>
+        </div>
+
+          <div class="d-flex justify-content-left py-2" id="name"><h5>Address:</h5></div>
+          <div class="d-flex justify-content-center input-group">
+            <div class="row align-items-center p-2x ">
+              <div class="row align-self-center">
+                <div class="col">
+                <input type="text" class="form-control" placeholder="street"/>
+                </div>
+                <div class="col">
+                <input type="text" class="form-control" placeholder="City"/>
+                </div>
+                <div class="col">
+                <input type="text" class="form-control" placeholder="Province"/>
+                </div>
+              </div> 
+            </div>
+          </div>
+
+          <div class="d-flex justify-content-left py-2" id="name"><h5>Date:</h5></div>
+          <div class="d-flex justify-content-left input-group">
+
+          <input type="date"></input>
+          </div>
+            
+          <div class="p-4">
+            <div class="row align-items-center p-2x ">
+            <h5>Civil Status:</h5>
+              <div class="col d-flex justify-content-center p-2">
+            <select name="status" id="cars">
+              <option value="Single">Single</option>
+              <option value="Married">Married</option>
+              <option value="Widow/er">Widow/er</option>
+              <option value="Divorced">Divorced</option>
+            </select>
+              </div>
+              <h5>Gender:</h5>
+              <div class="col d-flex">
+              <select name="status" id="gender">
+              <option value="Single">Female</option>
+              <option value="Married">Male</option>
+            </select>
+              </div>
+            </div>
+          </div>
+        
+      </div>
+    </div>
+  </div>
+</div>
+<!-- <div class="container h-100">
+  <div class="container">
+    <h2>Registration</h2>
+  </div>
+    <div class="container">
+    <form>
+      <div class="input-group">
+        <div class="row row align-items-center h-100">
+          <div class="col align-self-center">
+        <input type="text" class="form-control" id="firstname" placeholder="First Name"/>
+          </div>
+          <div class="col">
+        <input type="text" class="form-control" placeholder="Middle Name">
+          </div>
+          <div class="col">
+        <input type="text" class="form-control" placeholder="Last Name">
+          </div>
+      </div>
+    </form>
+    </div>
+    
+</div> -->
+
+</body>
+</html>
