d9fbc04adc78b4d681a1cebf1189b59315d26144
Kyle Olodin <kylemartinezolodin@gmail.com>
2020-09-20 08:39:42 +0800

    Removed File Duplications

--- EightBit_store/New folder/login/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import View
-
-# Create your views here.
-class LoginIndexView(View):
-    def get(self, request):
-        return render(request, 'login/index.html')
